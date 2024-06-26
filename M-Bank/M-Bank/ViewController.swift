e6ddc8cb0a919087fd971138138bb9ecbeda1acd
AntonPolovoy21 <a.polovoy@inbox.ru>
2024-06-26 17:38:15 +0300

    init...

---
+++ M-Bank/M-Bank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M-Bank
+//
+//  Created by Anton Polovoy on 26.06.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
